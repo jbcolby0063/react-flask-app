d163ed6237d16b4e44130ff97e2d23c8f30750d8
jongho <jbcolby0063@gmail.com>
2021-06-14 22:20:47 -0500

    add comments

--- api/api.py
+++ api/api.py
@@ -1,8 +1,9 @@
+# Flask API Project
 from flask import Flask
 import time
 
 app = Flask(__name__)
 
-@app.route('/time')
+@app.route('/time') # /time URL
 def get_current_time():
-    return {'time': time.time()}
+    return {'time': time.time()} # return response
--- src/App.js
+++ src/App.js
@@ -2,11 +2,15 @@ import React, { useState, useEffect } from 'react'
 import logo from './logo.svg';
 import './App.css';
 
+// In the first terminal, type "yarn start" --> React frontend running at http://localhost:3000
+// In the second terminal, type "yarn start-api" --> Flask backend running at http://localhost:5000
+
 function App() {
   const [currentTime, setCurrentTime] = useState(1)
 
-  useEffect(() => {
-    fetch("/time").then(res => res.json()).then(data => {
+  // send the request to Flask API through fetch() and convert the response to JavaScript Object through .json()
+  useEffect(() => { 
+    fetch("/time").then(res => res.json()).then(data => { // just use "/time" due to proxy configuration in package.json
       setCurrentTime(data.time)
     })
   }, [])   
